cca4f752f538fe01f5718e2eadca5e42be0910b8
cmatomic <33186553+cmatomic@users.noreply.github.com>
2020-08-25 00:24:02 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -118,11 +118,12 @@ find ./vlc-$VERSION/build/usr/lib/vlc/plugins/ -name "lib*.so*" -exec patchelf -
 
 wget "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x ./linuxdeployqt-continuous-x86_64.AppImage
-export UPDATE_INFORMATION="* New test release  "
-LINUX_DEPLOY_QT_EXCLUDE_COPYRIGHTS=true appimage-wrapper linuxdeployqt-continuous-x86_64.AppImage -u gh-releases-zsync|cmatomic|AppimageDEVtest|continuous|VLC_media_player$VERSION.zsync  vlc-$VERSION/build/org.videolan.vlc.desktop  -bundle-non-qt-libs -exclude-libs=libfreetype.so.6
-LINUX_DEPLOY_QT_EXCLUDE_COPYRIGHTS=true ARCH=x86_64 appimage-wrapper linuxdeployqt-continuous-x86_64.AppImage -u gh-releases-zsync|cmatomic|AppimageDEVtest|continuous|VLC_media_player$VERSION.zsync  vlc-$VERSION/build/org.videolan.vlc.desktop  -appimage
+#export UPDATE_INFORMATION="* New test release  "
+LINUX_DEPLOY_QT_EXCLUDE_COPYRIGHTS=true appimage-wrapper linuxdeployqt-continuous-x86_64.AppImage -g  vlc-$VERSION/build/org.videolan.vlc.desktop  -bundle-non-qt-libs -exclude-libs=libfreetype.so.6
+LINUX_DEPLOY_QT_EXCLUDE_COPYRIGHTS=true ARCH=x86_64 appimage-wrapper linuxdeployqt-continuous-x86_64.AppImage -g  vlc-$VERSION/build/org.videolan.vlc.desktop  -appimage
 
 mkdir -p release
 cp ./VLC_media_player*.zsync release/
 cp ./VLC_media_player*.AppImage release/
 md5sum ./VLC_media_player*.AppImage > release/MD5.txt
+#-u gh-releases-zsync|cmatomic|AppimageDEVtest|continuous|VLC_media_player$VERSION.zsync
