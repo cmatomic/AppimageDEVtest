34e551f1db64ac4cb66c266e4d301781adda6dc6
cmatomic <33186553+cmatomic@users.noreply.github.com>
2020-09-22 07:32:05 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -50,6 +50,19 @@ export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig
 
 (
 
+wget https://github.com/intel/intel-vaapi-driver/releases/download/2.4.1/intel-vaapi-driver-2.4.1.tar.bz2
+tar -xvjf intel-vaapi-driver-2.4.1.tar.bz2
+cd intel-vaapi-driver-2.4.1
+ ./configure
+ make -j$(nproc)
+ make -j$(nproc) install
+
+
+)
+
+
+(
+
 wget https://github.com/intel/libva/releases/download/2.8.0/libva-2.8.0.tar.bz2
 tar -xvjf libva-2.8.0.tar.bz2
 cd libva-2.8.0
@@ -170,7 +183,7 @@ chmod a+x ./linuxdeployqt-continuous-x86_64.AppImage
 chmod a+x aw
 AW=./aw
 LINUX_DEPLOY_QT_EXCLUDE_COPYRIGHTS=true ARCH=x86_64 $AW  linuxdeployqt-continuous-x86_64.AppImage vlc-$VERSION/build/org.videolan.vlc.desktop  -bundle-non-qt-libs -exclude-libs=libfreetype.so.6 
-LINUX_DEPLOY_QT_EXCLUDE_COPYRIGHTS=true ARCH=x86_64 $AW  linuxdeployqt-continuous-x86_64.AppImage  vlc-$VERSION/build/org.videolan.vlc.desktop -updateinformation="gh-releases-zsync|cmatomic|AppimageDEVtest|$VERSION|VLC_media_player*.AppImage.zsync" -appimage
+LINUX_DEPLOY_QT_EXCLUDE_COPYRIGHTS=true ARCH=x86_64 $AW  linuxdeployqt-continuous-x86_64.AppImage  vlc-$VERSION/build/org.videolan.vlc.desktop -updateinformation="gh-releases-zsync|cmatomic|AppimageDEVtest|testing|VLC_media_player*.AppImage.zsync" -appimage
 mkdir -p release
 cp ./VLC_media_player*.zsync release/
 cp ./VLC_media_player*.AppImage release/
